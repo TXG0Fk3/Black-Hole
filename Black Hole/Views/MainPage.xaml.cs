8ba432a0777a22d55fdf783c8cf7bdcd56dff836
TXG0Fk3 <leovertonx12@outlook.com>
2025-01-02 23:23:55 -0300

    Upl

---
+++ Black Hole/Views/MainPage.xaml.cs
@@ -0,0 +1,13 @@
+using Microsoft.UI.Xaml.Controls;
+
+
+namespace Black_Hole.Views
+{
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+    }
+}
