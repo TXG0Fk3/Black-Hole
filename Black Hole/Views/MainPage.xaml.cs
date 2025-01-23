48f18d14fb59df4df5f21edbcefc8629e6391800
TXG0Fk3 <leovertonx12@outlook.com>
2025-01-23 14:17:34 -0300

    Added Navigation Footer Bar functions

--- Black Hole/Views/MainPage.xaml.cs
+++ Black Hole/Views/MainPage.xaml.cs
@@ -1,4 +1,6 @@
 using Microsoft.UI.Xaml.Controls;
+using Microsoft.UI.Xaml.Controls.Primitives;
+using System.Linq;
 
 
 namespace Black_Hole.Views
@@ -8,6 +10,20 @@ namespace Black_Hole.Views
         public MainPage()
         {
             this.InitializeComponent();
+        }
+
+        private void ToggleButton_Click(object sender, Microsoft.UI.Xaml.RoutedEventArgs e)
+        {
+            ToggleButton TB_Sender = sender as ToggleButton;
+            TB_Sender.IsChecked = true;
+
+            foreach (ToggleButton i in Footer.Children.OfType<ToggleButton>())
+            {
+                if (i != TB_Sender)
+                {
+                    i.IsChecked = false;
+                }
+            }
         }
     }
 }
