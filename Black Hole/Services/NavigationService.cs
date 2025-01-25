ebd92e2244de6c3b5a537d9af6ba3a68acbc05fb
TXG0Fk3 <leovertonx12@outlook.com>
2025-01-25 02:59:36 -0300

    New Navigation Service

---
+++ Black Hole/Services/NavigationService.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.UI.Xaml.Controls;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Black_Hole.Services
+{
+    public class NavigationService
+    {
+        private static NavigationService _instance;
+        private Frame _frame;
+
+        private NavigationService() { }
+
+        public static NavigationService Instance
+        {
+            get
+            {
+                if (_instance == null)
+                {
+                    _instance = new NavigationService();
+                }
+                return _instance;
+            }
+        }
+
+        public void Initialize(Frame frame)
+        {
+            _frame = frame;
+        }
+
+        public void NavigateTo(Type pageType, object parameter = null)
+        {
+            _frame.Navigate(pageType, parameter);
+        }
+
+        public void GoBack()
+        {
+            if (_frame.CanGoBack)
+            {
+                _frame.GoBack();
+            }
+        }
+    }
+}
