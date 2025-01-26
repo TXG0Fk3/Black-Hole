7a0ff08cdb19d7637206e933b8da600c247fe0dd
TXG0Fk3 <leovertonx12@outlook.com>
2025-01-26 14:58:36 -0300

    Now can adapt UI to PageContext

--- Black Hole/Views/PendingPage.xaml.cs
+++ Black Hole/Views/PendingPage.xaml.cs
@@ -1,31 +1,119 @@
-using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
-using Windows.Foundation;
-using Windows.Foundation.Collections;
+using Black_Hole.Enums;
 using Microsoft.UI.Xaml;
 using Microsoft.UI.Xaml.Controls;
-using Microsoft.UI.Xaml.Controls.Primitives;
-using Microsoft.UI.Xaml.Data;
-using Microsoft.UI.Xaml.Input;
-using Microsoft.UI.Xaml.Media;
 using Microsoft.UI.Xaml.Navigation;
-
-// To learn more about WinUI, the WinUI project structure,
-// and more about our project templates, see: http://aka.ms/winui-project-info.
+using System;
 
 namespace Black_Hole.Views
 {
-    /// <summary>
-    /// An empty page that can be used on its own or navigated to within a Frame.
-    /// </summary>
     public sealed partial class PendingPage : Page
     {
+        private Tuple<PendingType, String> _pageContext;
+
         public PendingPage()
         {
             this.InitializeComponent();
+        }
+
+        protected async override void OnNavigatedTo(NavigationEventArgs e)
+        {
+            base.OnNavigatedTo(e);
+
+            if (e.Parameter is Tuple<PendingType, string> parameter)
+            {
+                _pageContext = parameter;
+                ProcessPageContext();
+            }
+        }
+
+        // Altera a page de acordo com o tipo de espera (send ou receive)
+        private void ProcessPageContext()
+        {
+            string value = _pageContext.Item2;
+
+            switch (_pageContext.Item1)
+            {
+                case PendingType.SenderWaitingReceiverAcceptance:
+                    Title.Text = "Your Transmission Code";
+                    FileDescription.Text = "Test";
+
+                    var stackPanel = new StackPanel()
+                    {
+                        Margin = new Thickness(0, 12, 0, 0),
+                        HorizontalAlignment = HorizontalAlignment.Center,
+                        VerticalAlignment = VerticalAlignment.Center,
+                        Orientation = Orientation.Horizontal,
+                        Spacing = 10
+                    };
+
+                    stackPanel.Children.Add
+                    (
+                        new TextBox()
+                        {
+                            Width = 260,
+                            IsReadOnly = true,
+                            Text = "test"
+                        }
+                    );
+
+                    stackPanel.Children.Add
+                    (
+                        new Button
+                        {
+                            Width = 35,
+                            Height = 35,
+                            Padding = new Thickness(0),
+                            Content = new FontIcon()
+                            {
+                                Glyph = "\uE8C8"
+                            }
+                        }
+                    );
+
+                    PageVariableContent.Children.Add(stackPanel);
+                    PageVariableContent.Children.Add
+                    (
+                        new TextBlock()
+                        {
+                            Margin = new Thickness(0, 12, 0, 0),
+                            HorizontalAlignment = HorizontalAlignment.Center,
+                            FontSize = 16,
+                            Style = (Style)Application.Current.Resources["BodyTextBlockStyle"],
+                            Text = "Send the Code to the Receiver."
+                        }
+                    );
+
+                    break;
+
+                case PendingType.ReceiverRespondingToConfirmation:
+                    Title.Text = "Accept File?";
+                    FileDescription.Text = "Test";
+
+                    PageVariableContent.Children.Add
+                    (
+                        new Button()
+                        {
+                            Width = 200,
+                            Height = 45,
+                            HorizontalAlignment = HorizontalAlignment.Center,
+                            Content = "Accept",
+                            Style = (Style)Application.Current.Resources["AccentButtonStyle"]
+                        }
+                    );
+
+                    PageVariableContent.Children.Add
+                    (
+                        new Button()
+                        {
+                            Width = 200,
+                            Height = 45,
+                            HorizontalAlignment = HorizontalAlignment.Center,
+                            Content = "Cancel"
+                        }
+                    );
+
+                    break;
+            }
         }
     }
 }
