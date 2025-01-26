ec8739410185dbf6f182e752f89365b8572a3e40
TXG0Fk3 <leovertonx12@outlook.com>
2025-01-26 16:51:19 -0300

    QrCode Generator helper

---
+++ Black Hole/Helpers/QrCodeHelper.cs
@@ -0,0 +1,16 @@
+﻿using QRCoder;
+using System.Drawing;
+
+namespace Black_Hole.Helpers
+{
+    static class QrCodeHelper
+    {
+        public static Bitmap GenerateQrCode(string text)
+        {
+            QRCodeGenerator qrGenerator = new();
+            QRCodeData qrCodeData = qrGenerator.CreateQrCode(text, QRCodeGenerator.ECCLevel.Q);
+
+            return new QRCode(qrCodeData).GetGraphic(20);
+        }
+    }
+}
