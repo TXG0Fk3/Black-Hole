2bd91dc28cf1cd71300b47aec1eb0a00a794e06f
TXG0Fk3 <leovertonx12@outlook.com>
2025-01-26 14:08:44 -0300

    Enum for PageContext

---
+++ Black Hole/Enums/PendingType.cs
@@ -0,0 +1,8 @@
+﻿namespace Black_Hole.Enums
+{
+    public enum PendingType
+    {
+        SenderWaitingReceiverAcceptance, // Indica que o remetente está aguardando que o receptor aceite o envio
+        ReceiverRespondingToConfirmation // Indica que o receptor está respondendo a confirmação (aceitando ou negando)
+    }
+}
