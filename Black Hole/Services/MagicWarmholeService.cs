31bfb9bc3fc0e48cf5e8c7b5caf3496441953051
TXG0Fk3 <leovertonx12@outlook.com>
2025-02-15 20:58:08 -0300

    Created new class and Initial implementation

---
+++ Black Hole/Services/MagicWarmholeService.cs
@@ -0,0 +1,39 @@
+﻿using System.IO;
+
+namespace Black_Hole.Services
+{
+    internal class MagicWarmholeService
+    {
+        private string _filePath;
+        private string _code;
+        public string FileName { get; private set; }
+        public string FileSize { get; private set; }
+
+        public MagicWarmholeService(string filePathOrCode)
+        {
+            if (File.Exists(filePathOrCode))
+            {
+                _filePath = filePathOrCode;
+
+                var fileInfo = new FileInfo(_filePath);
+                FileName = fileInfo.Name;
+                FileSize = fileInfo.Length.ToString();
+            }
+            else
+            {
+                _code = filePathOrCode;
+                // To do: Implementar a lógica para coletar informações do arquivo a ser recebido
+            }
+        }
+
+        public void SendFile()
+        {
+            // To Do: Implementar a lógica para enviar o arquivo
+        }
+
+        public void ReceiveFile()
+        {
+            // To Do: Implementar a lógica para receber o arquivo
+        }
+    }
+}
