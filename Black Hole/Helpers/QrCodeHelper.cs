3a791ccba71c5a2cbb3751243628d76c31c1d163
TXG0Fk3 <leovertonx12@outlook.com>
2025-02-15 23:49:37 -0300

    now return path to qrcode && Can specify path to save

--- Black Hole/Helpers/QrCodeHelper.cs
+++ Black Hole/Helpers/QrCodeHelper.cs
@@ -1,16 +1,29 @@
 ﻿using QRCoder;
-using System.Drawing;
+using System.IO;
 
 namespace Black_Hole.Helpers
 {
     static class QrCodeHelper
     {
-        public static Bitmap GenerateQrCode(string text)
+        public static string GenerateQrCode(string text, string? folderPath = null)
         {
+            string filePath;
+
+            if (folderPath != null)
+            {
+                Directory.CreateDirectory(folderPath);
+                filePath = $"{folderPath}\\qrCode.png";
+            }
+            else
+            {
+                filePath = Path.GetTempPath() + "qrCode.png";
+            }
+
             QRCodeGenerator qrGenerator = new();
             QRCodeData qrCodeData = qrGenerator.CreateQrCode(text, QRCodeGenerator.ECCLevel.Q);
+            new QRCode(qrCodeData).GetGraphic(20).Save(filePath);
 
-            return new QRCode(qrCodeData).GetGraphic(20);
+            return filePath;
         }
     }
 }
