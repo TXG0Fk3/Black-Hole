ac62d36ad1c8b9e6d45f92f7f0d0a5b09274ba62
TXG0Fk3 <leovertonx12@outlook.com>
2025-02-24 16:33:31 -0300

    Added FilePickerHelder class

---
+++ Black Hole/Helpers/FilePickerHelper.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.UI.Xaml;
+using System;
+using System.Threading.Tasks;
+using Windows.Storage.Pickers;
+using WinRT.Interop;
+
+namespace Black_Hole.Helpers
+{
+    static class FilePickerHelper
+    {
+        private static void InitializePicker(dynamic picker, Window window)
+        {
+            picker.FileTypeFilter.Add("*");
+            InitializeWithWindow.Initialize(picker, WindowNative.GetWindowHandle(window));
+        }
+
+        public async static Task<string?> PickFile(Window window)
+        {
+            var openPicker = new FileOpenPicker();
+
+            InitializePicker(openPicker, window);
+
+            var file = await openPicker.PickSingleFileAsync();
+
+            return file?.Path;
+        }
+
+        public async static Task<string?> PickFolder(Window window)
+        {
+            var openPicker = new FolderPicker();
+
+            InitializePicker(openPicker, window);
+
+            var folder = await openPicker.PickSingleFolderAsync();
+
+            return folder?.Path;
+        }
+    }
+}
