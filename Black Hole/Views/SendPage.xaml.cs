f3835edaf9a213af5151fa0c59d760732a128661
TXG0Fk3 <leovertonx12@outlook.com>
2025-02-24 17:50:02 -0300

    Implemented

--- Black Hole/Views/SendPage.xaml.cs
+++ Black Hole/Views/SendPage.xaml.cs
@@ -1,31 +1,39 @@
-using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
-using Windows.Foundation;
-using Windows.Foundation.Collections;
+using Black_Hole.Enums;
+using Black_Hole.Helpers;
+using Black_Hole.Services;
 using Microsoft.UI.Xaml;
 using Microsoft.UI.Xaml.Controls;
-using Microsoft.UI.Xaml.Controls.Primitives;
-using Microsoft.UI.Xaml.Data;
-using Microsoft.UI.Xaml.Input;
-using Microsoft.UI.Xaml.Media;
-using Microsoft.UI.Xaml.Navigation;
+using System;
 
-// To learn more about WinUI, the WinUI project structure,
-// and more about our project templates, see: http://aka.ms/winui-project-info.
 
 namespace Black_Hole.Views
 {
-    /// <summary>
-    /// An empty page that can be used on its own or navigated to within a Frame.
-    /// </summary>
     public sealed partial class SendPage : Page
     {
         public SendPage()
         {
             this.InitializeComponent();
+        }
+
+        private async void FileButton_Click(object sender, RoutedEventArgs e)
+        {
+            HandlePathSelection(await FilePickerHelper.PickFile(App.MainWindow));
+        }
+
+        private async void FolderButton_Click(object sender, RoutedEventArgs e)
+        {
+            HandlePathSelection(await FilePickerHelper.PickFolder(App.MainWindow));
+        }
+
+        private void HandlePathSelection(string? Path)
+        {
+            if (Path != null)
+            {
+                MagicWormholeService magicWormholeService = new(Path);
+                NavigationService.Instance.NavigateTo(
+                    typeof(PendingPage),
+                    new Tuple<PendingType, MagicWormholeService>(PendingType.SenderWaitingReceiverAcceptance, magicWormholeService));
+            }
         }
     }
 }
